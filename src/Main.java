4114d081bc75025406d60a1b081100e122ba73ef
Abhijeet <abhigulati1000@gmail.com>
2023-07-26 11:43:26 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    //1D DP
+    //HOUSE ROBBER 2
+    public int rob(int[] nums) {
+
+    }
+}
